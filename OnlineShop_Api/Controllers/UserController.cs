28e13bf2cf71667c0951f6efabe866e9c0eb2e66
hbsamur <hbsamur@YAZILIM3>
2024-01-15 15:42:34 +0300

    logut eklendi

---
+++ OnlineShop_Api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public UserController(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+
+            return Ok();
+        }
+    }
+}
